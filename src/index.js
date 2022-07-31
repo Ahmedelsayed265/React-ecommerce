1b73da4b50333c7986737ba1c4bec75a2a8843f2
Ahmed elsayed <87664326+Ahmedelsayed265@users.noreply.github.com>
2022-07-31 15:06:44 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
 
 import App from "./app";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.querySelector("#root")
+);
