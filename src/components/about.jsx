71903c9cac9a39b6329e1da174a738e222a337c9
Ahmed elsayed <87664326+Ahmedelsayed265@users.noreply.github.com>
2022-08-02 18:56:50 +0200

    1

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
+import Team from "./team";
+import Company from "./company";
 class About extends Component {
   state = {};
   render() {
@@ -8,15 +10,18 @@ class About extends Component {
         <aside className="col-4">
           <ul>
             <li>
-              <Link to="/about/team">Our Team</Link>
+              <Link to="/about">Our Company</Link>
             </li>
             <li>
-              <Link to="/about/company">Our Company</Link>
+              <Link to="/about/team">Our Team</Link>
             </li>
           </ul>
         </aside>
         <div className="col-8">
-        
+          <Switch>
+            <Route path="/about/team" component={Team} />
+            <Route path="/about" exact component={Company} />
+          </Switch>
         </div>
       </div>
     );
