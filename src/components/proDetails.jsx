f00053c6afa8d936d0b800ac3ddd341fb2c6d117
Ahmed elsayed <87664326+Ahmedelsayed265@users.noreply.github.com>
2022-08-02 17:46:05 +0200

    1

--- src/components/proDetails.jsx
+++ src/components/proDetails.jsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import qs from "query-string";
-const Details = (props) => {
-  let res = qs.parse(props.location.search);
-  console.log(res);
-  const product = props.products.filter(
-    (c) => c.id === parseInt(props.match.params.id)
-  )[0];
-  return (
-    <React.Fragment>
-      <h1>Product No.{product.id} Details</h1>
-      <h2>{product.name}</h2>
-      <h2>Count in shopping cart is {product.count}</h2>
-    </React.Fragment>
-  );
-};
-
+class Details extends Component {
+  state = {};
+  render() {
+    let res = qs.parse(this.props.location.search);
+    console.log(res);
+    const product = this.props.products.filter(
+      (c) => c.id === parseInt(this.props.match.params.id)
+    )[0];
+    return (
+      <React.Fragment>
+        <h1>Product No.{product.id} Details</h1>
+        <h2>{product.name}</h2>
+        <h2>Count in shopping cart is {product.count}</h2>
+        <button className="btn bg-info btn-sm fs-4" style={{ color: "white" }}>
+          Save
+        </button>
+      </React.Fragment>
+    );
+  }
+}
 export default Details;
