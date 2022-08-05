53b83bc8a1c1a731b234fbc985ccb89ca4fd7cb2
Ahmed elsayed <87664326+Ahmedelsayed265@users.noreply.github.com>
2022-08-05 22:13:41 +0200

    1

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,33 +1,30 @@
-import React, { Component } from "react";
 import Add from "./addcart";
-class Menu extends Component {
-  render() {
-    const { products, onCartChange } = this.props;
-    return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Name</th>
-            <th scope="col">Price</th>
-            <th scope="col"></th>
+const Menu = (props) => {
+  const { products, onCartChange } = props;
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Name</th>
+          <th scope="col">Price</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((product) => (
+          <tr key={product.id}>
+            <th scope="row">{product.id}</th>
+            <td>{product.name}</td>
+            <td>{product.price}$</td>
+            <td>
+              <Add onCartChange={onCartChange} product={product} />
+            </td>
           </tr>
-        </thead>
-        <tbody>
-          {products.map((product) => (
-            <tr key={product.id}>
-              <th scope="row">{product.id}</th>
-              <td>{product.name}</td>
-              <td>{product.price}$</td>
-              <td>
-                <Add onCartChange={onCartChange} product={product} />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  }
-}
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default Menu;
