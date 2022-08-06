5f4bc58b30aee221ebae208197cdad220fcab385
Ahmed elsayed <87664326+Ahmedelsayed265@users.noreply.github.com>
2022-08-06 23:16:13 +0200

    1

--- src/components/proForm.jsx
+++ src/components/proForm.jsx
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
 class productform extends Component {
-  state = {};
+  state = {
+    name: "",
+    price: "",
+  };
+  onSave = (e) => {
+    e.preventDefault();
+    console.log("saved");
+  };
+  change = (e) => {
+    let state = { ...this.state };
+    state[e.currentTarget.name] = e.currentTarget.value;
+    this.setState(state);
+  };
   render() {
     return (
       <React.Fragment>
@@ -9,18 +21,32 @@ class productform extends Component {
         ) : (
           <h1 className="text-center">Edit Product</h1>
         )}
-        <form>
+        <form onSubmit={this.onSave}>
           <div className="mb-3">
             <label htmlFor="name" className="form-label">
               Name
             </label>
-            <input type="text" className="form-control" id="name" />
+            <input
+              name="name"
+              value={this.state.name}
+              onChange={this.change}
+              type="text"
+              className="form-control"
+              id="name"
+            />
           </div>
           <div className="mb-3">
             <label htmlFor="price" className="form-label">
               Price
             </label>
-            <input type="text" className="form-control" id="price" />
+            <input
+              name="price"
+              value={this.state.price}
+              onChange={this.change}
+              type="text"
+              className="form-control"
+              id="price"
+            />
           </div>
           <button type="submit" className="btn btn-primary">
             Save
